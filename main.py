820f6df44aa2e5fec910deda5cc7ef0e02b577c4
captattar01 <73378170+captattar01@users.noreply.github.com>
2020-12-07 20:20:28 +0700

    Update main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# ------------------------ Modules ------------------------ #
+
+import discord
+import asyncio
+import random
+import datetime
+import os
+import json
+import PIL
+import math
+import pyfiglet
+import requests
+import re
+import base64
+import dyv_math as mfmath
+from boto.s3.connection import S3Connection
+from PIL import Image
+from io import BytesIO
+from discord.ext import commands
