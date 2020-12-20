9e9a40cb841cf07542221b20d14dcd5d85d824c5
mutefx <58331876+MangoHD@users.noreply.github.com>
2020-12-20 12:46:20 +0700

    .

--- cogs/ErrorHandler.py
+++ cogs/ErrorHandler.py
@@ -1,20 +1,17 @@
 import discord
 import os
 import asyncio
-import json
 from discord.ext import commands
-from bot_things import  motd, emcolor, ercolor, footerd, getprefix, get_prefix
 
-def prefix(message):
-    with open("./configs/prefixes.json", "r") as f:
-        prefixes = json.load(f)
-
-    if str(message.guild.id) not in prefixes:
-        return 'z!'
-
-    else:
-        prefix = prefixes[str(message.guild.id)]
-        return prefix
+getprefix = ['z!', 'Z!']
+prefix = 'z!'
+motd = 'Newly made bot :D'
+footer = 'Zeptagon'
+emcolor = 0x777777
+ercolor = 0xff0000
+def footerd(emb):
+    emb.add_field(name='_ _', value='Links: [Support Server](https://discord.gg/89eu5WD)・[Invite Me](https://discord.com/oauth2/authorize?client_id=785496485659148359&permissions=8&scope=bot)')
+    emb.set_footer(text='Zeptagon', icon_url='https://cdn.discordapp.com/avatars/785496485659148359/0fc85eb060bb37c35726fabe791170fe.webp?size=1024')
 
 class ErrorHandler(commands.Cog):
     def __init__(self, bot):
@@ -22,15 +19,15 @@ class ErrorHandler(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        if isinstance(error, discord.ext.commands.errors.CommandNotFound):
-            await ctx.message.delete()
-            embed=discord.Embed(
-                title='Error',
-                description=f'`{error}`\n`{prefix(ctx.message)}help` for a list of commands.',
-                colour=emcolor
-            )
-            footerd(embed)
-            await ctx.send(f"```{error}\nUse {prefix(ctx.message)}help for info about commands.```", delete_after=35)
+        # if isinstance(error, discord.ext.commands.errors.CommandNotFound):
+        #     await ctx.message.delete()
+        #     embed=discord.Embed(
+        #         title='Error',
+        #         description=f'`{error}`\n`{prefix}help` for a list of commands.',
+        #         colour=emcolor
+        #     )
+        #     footerd(embed)
+        #     await ctx.send(f"```{error}\nUse {prefix}help for info about commands.```", delete_after=35)
 
         if isinstance(error, commands.MissingRequiredArgument):
             embed=discord.Embed(
